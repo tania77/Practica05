be04c180fc2b99e28dadc4de244f2c4e0fdcaaa6
Tania González Rodríguez <ainatrg@gmail.com>
2015-10-27 16:21:16 +0000

    v2

--- lib/racionales.rb
+++ lib/racionales.rb
@@ -8,4 +8,34 @@ class Racionales
 	def to_s
 		"#{@numerador}/#{@denominador}"
 	end
+	
+	def suma(object)
+		aux = mcm(@denominador, object.denominador)
+		resultado = Racionales.new(((aux / @denominador) * @numerador)+((aux / object.denominador) * object.numerador),aux )
+	end
+	def resta
+		aux = mcm(@denominador, object.denominador)
+		resultado = Racionales.new(((aux / @denominador) * @numerador)-((aux / object.denominador) * object.numerador),aux )
+	end
+	
+	def multiplicacion(object)
+		resultado= Racionales.new(@numerador * object.numerador, @denominador * object.denominador)
+	end
+	
+	def division(object)
+		resultado= Racionales.new(@numerador * object.denominador, @denominador * object.numerador)
+	end
+	
+	def mcm(a,b)
+		aux = mcd(a,b)
+		(a/aux)* b
+	end
+	
+	def mcd(a,b)
+		#u, v = u.abs, v.abs
+		while b != 0
+			a, b = a, a%b
+		end
+		a
+	end
 end
--- test/tc_racionales.rb
+++ test/tc_racionales.rb
@@ -10,4 +10,20 @@ class TestRacionales < Test::Unit::TestCase
 		assert_equal("3/5", @numa.to_s)
 		assert_equal("5/3", @numb.to_s)
 	end
+	
+	def test_multiplicacion
+		assert_equal("15/15", @numa.multiplicacion(@numb).to_s)
+	end
+	
+	def test_division
+		assert_equal("9/25", @numa.division(@numb).to_s)
+	end
+	
+	def test_suma
+		assert_equal("34/15", @numa.suma(@numb).to_s)
+	end
+	
+	def test_resta
+		assert_equal("-11/15", @numa.resta(@numb).to_s)
+	end
 end
